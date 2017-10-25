c537d15127497703656a5a7a33640d779e5d7761
zhouyunqiang <1204364027@qq.com>
2017-10-25 17:35:59 +0800

    0

---
+++ cmake-build-debug/CMakeFiles/cpp_workdir.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_workdir.dir/main.cpp.obj"
+  "cpp_workdir.pdb"
+  "cpp_workdir.exe"
+  "cpp_workdir.exe.manifest"
+  "libcpp_workdir.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_workdir.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    //using namespace std;
+    cout << "helloworld"<< endl;
+    cin.get();//in order to see the output
+    return 0;
+
+}
